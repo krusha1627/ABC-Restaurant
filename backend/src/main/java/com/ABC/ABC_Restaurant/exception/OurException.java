c05b084de3aee3966ceaf24dff7b62a1726b5572
Krusha Paheerathan <krusha1627@gmail.com>
2024-09-06 20:03:52 +0530

    backend commit

---
+++ backend/src/main/java/com/ABC/ABC_Restaurant/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.ABC.ABC_Restaurant.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/ABC/ABC_Restaurant/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ABC.ABC_Restaurant.repo;
+
+import com.ABC.ABC_Restaurant.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
